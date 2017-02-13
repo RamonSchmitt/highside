04afb39cd90b04026c1430470de7d4ab360a69f8
RamonSchmitt <ramon.schmitt@yahoo.com>
2017-02-13 09:05:27 +0100

    final

--- src/components/app.js
+++ src/components/app.js
@@ -2,87 +2,87 @@ import React, { Component } from 'react';
 import BlogList from './blog-list';
 
 export default class App extends Component {
-  constructor() {
-    super();
+ constructor() {
+   super();
 
-    this.state = {
-      data: [],
-      post: {
-        title: "",
-        excerpt: "",
-        body: ""
-      }
-    }
-  }
+   this.state = {
+     data: [],
+     post: {
+       title: '',
+       excerpt: '',
+       body: ''
+     }
+   }
+ }
 
-  componentDidMount() {
-    this.setState({
-      data: [{
-        title: "Blog Title 1",
-        excerpt: "Fragment 1",
-        body: "This is the text of blog entry"
-      },{
-        title: "Blog Title 2",
-        excerpt: "Fragment 2",
-        body: "Text of blog 2"
-      }]
-    });
-  }
+ componentDidMount() {
+   this.setState({
+     data: [{
+       title: "Blog Title 1",
+       excerpt: "Fragment 1",
+       body: "This is the text of blog entry"
+     },{
+       title: "Blog Title 2",
+       excerpt: "Fragment 2",
+       body: "Text of blog 2"
+     }]
+   });
+ }
 
-  onTitleChange(event) {
-    this.setState({post: { ...this.state.post, title: event.target.value }});
-  }
+ onTitleChange(event) {
+   this.setState({post: { ...this.state.post, title: event.target.value } })
+ }
 
-  onExcerptChange(event) {
-    this.setState({post: { ...this.state.post, excerpt: event.target.value }});
-  }
+ onExcerptChange(event) {
+   this.setState({post: { ...this.state.post, excerpt: event.target.value } })
+ }
 
-  onBodyChange(event) {
-    this.setState({post: { ...this.state.post, body: event.target.value }});
-  }
+ onBodyChange(event) {
+   this.setState({post: { ...this.state.post, body: event.target.value } })
+ }
 
-  handleSubmit(event) {
-    event.preventDefault();
+ handleSubmit(event) {
+   event.preventDefault();
+   this.setState({
+     data: [...this.state.data, this.state.post],
+     post: {
+       title: '',
+       excerpt: '',
+       body: ''
+     }
+   })
+ }
 
-    this.state.data.push(this.state.post);
-    console.log(this.state.data);
+ render() {
+   console.log(this.state)
+   return (
+     <div className="container">
+       <form className="input-group">
+         <input
+           type="text"
+           placeholder="Title"
+           className="form-control"
+           value={this.state.post.title}
+           onChange={event => this.onTitleChange(event)} />
 
-    this.refs.inputTitle.value="";
-    this.refs.inputExcerpt.value="";
-    this.refs.inputBody.value="";
-  }
+         <input
+           type="text"
+           placeholder="Description"
+           className="form-control"
+           value={this.state.post.excerpt}
+           onChange={event => this.onExcerptChange(event)} />
 
-  render() {
-    return (
-      <div className="container">
-        <form className="input-group">
-          <input
-            ref="inputTitle"
-            type="text"
-            placeholder="Title"
-            className="form-control"
-            onChange={event => this.onTitleChange(event)} />
+         <input
+           type="text"
+           placeholder="Your Story"
+           className="form-control"
+           value={this.state.post.body}
+           onChange={event => this.onBodyChange(event)} />
 
-          <input
-            ref="inputExcerpt"
-            type="text"
-            placeholder="Description"
-            className="form-control"
-            onChange={event => this.onExcerptChange(event)} />
-
-          <input
-            ref="inputBody"
-            type="text"
-            placeholder="Your Story"
-            className="form-control"
-            onChange={event => this.onBodyChange(event)} />
-
-          <span className="input-group-btn">
-            <button type="button" onClick={event => this.handleSubmit(event)} className="btn btn-secondary">Add Blog</button>
-          </span>
-        </form>
-        <BlogList data={this.state.data}/>
-      </div>
-    );
-  }
+         <button onClick={event => this.handleSubmit(event)} type="button" className="btn btn-secondary">Add Blog</button>
+       </form>
+       <BlogList data={this.state.data}/>
+     </div>
+   );
+ }
 }
--- src/components/blog.js
+++ src/components/blog.js
@@ -3,21 +3,11 @@ import React, { Component } from 'react';
 export default class Blog extends Component {
   render() {
     return (
-      <table className="table table-responsive">
-        <thead>
-          <tr>
-            <th>{this.props.title}</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>
-              <h6>{this.props.excerpt}</h6>
-              <p>{this.props.body}</p>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div>
+        <h1>{this.props.title}</h1>
+        <h3>{this.props.excerpt}</h3>
+        <p>{this.props.body}</p>
+      </div>
     );
   }
 }
